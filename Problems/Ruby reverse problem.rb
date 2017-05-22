7a246bf83ae57d4432f429d704b9d16cd2849425
Justin Hernandez <kaspersky102@aol.com>
2017-05-22 16:32:26 -0400

    completed layout for reverse problem

--- Problems/Ruby reverse problem.rb
+++ Problems/Ruby reverse problem.rb
@@ -1,18 +1,14 @@
 # Ruby Challenge: Reverse a given input such as the string "Hello World!" to result in "!dlroW olleH"
 
-#input = "Hello World!"
-
 # this following puts statement will do all of the work for you(puts input.reverse), but, there is an alternative way to do this which produce the same result
-
+# try not to "cheat" with the reverse method :)
 
 
 def reverser(input)
-	reversed_ary = []
-	normal_ary = input.split(//)
-	for char in normal_ary.length
-		p char
-	end
-	p reversed_ary
+	# Write the necessary code in here!
 end
 
 reverser("Hello World!")
+#make sure that this method call to reverser will return "!dlroW olleH"
+reverser("I am a great programmer")
+#make sure that this method call to reverser will return "remmargorp taerg a ma I"
